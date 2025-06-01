fc7f2f8ed1409873a04b259328da451f9bfe500c
James Garcia <conectis360@gmail.com>
2025-06-01 19:29:02 -0300

    feat: timing agent

--- tools/timing_agent/agent.py
+++ tools/timing_agent/agent.py
@@ -5,18 +5,18 @@ from google.adk.tools.agent_tool import AgentTool
 from . import prompt
 from .tools.google_document_tool import create_document
 
-name="ted_script_coordinator",
+name="timing_agent",
 model="gemini-2.0-flash",
     
 root_agent = Agent(
         name=name,
         model=model,
-        description="O agente coordenador principal. Processa documentos e delega tarefas específicas para especialistas.",
-        instruction=prompt.TED_TALK_ORCHESTRATOR_PROMPT,
+        description="O agente de timing. Processa a apresentação buscando deixar ela no timing.",
+        instruction=prompt.TIMING_AGENT_PROMPT,
         
         # O agente principal ainda precisa da ferramenta de processamento para sua tarefa core
-        tools=[create_document.create_google_document],
+        tools=[],
         
         # MUDANÇA CHAVE: Liga os sub-agentes aqui!
-        sub_agents=[content_analyzer_agent, storyteller_agent, timing_agent, visual_agent, personalizer_agent]
+        sub_agents=[]
 )
